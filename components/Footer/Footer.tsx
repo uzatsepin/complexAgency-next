07aba29158a2e5708382b02ad92520ed84067474
Andrii Zatsepin <uzatsepin@gmail.com>
2024-08-12 16:27:21 +0300

    added links to footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -17,24 +17,24 @@ export default function Footer() {
             </div>
 
             <div className="flex gap-6 md:gap-8 justify-center mt-10 md:mt-14 bg-zinc-800 md:bg-transparent py-4 md:py-0 border-t border-[#ffffff]/20 border-b md:border-transparent">
-                <div className="p-2 md:p-4 w-10 h-10 md:h-fit md:w-fit bg-white rounded-full outer-circle hover:scale-125 cursor-pointer transition-all duration-300 hover:bg-[#2CEEC2] group">
+                <a href='#' className="p-2 md:p-4 w-10 h-10 md:h-fit md:w-fit bg-white rounded-full outer-circle hover:scale-125 cursor-pointer transition-all duration-300 hover:bg-[#2CEEC2] group">
                     <Icon icon="f7:person-2-fill"  className="text-black h-6 w-6 md:h-8 md:w-8"/>
-                </div>
-                <div className="p-2 md:p-4 w-10 h-10 md:h-fit md:w-fit bg-white rounded-full outer-circle hover:scale-125 cursor-pointer transition-all duration-300 hover:bg-[#2CEEC2] group">
+                </a>
+                <a href='#' className="p-2 md:p-4 w-10 h-10 md:h-fit md:w-fit bg-white rounded-full outer-circle hover:scale-125 cursor-pointer transition-all duration-300 hover:bg-[#2CEEC2] group">
                     <Icon icon="ri:behance-fill" className="text-black h-6 w-6 md:h-8 md:w-8"/>
-                </div>
-                <div className="p-2 md:p-4 w-10 h-10 md:h-fit md:w-fit bg-white rounded-full outer-circle hover:scale-125 cursor-pointer transition-all duration-300 hover:bg-[#2CEEC2] group">
+                </a>
+                <a href='https://t.me/thecomplex_agency_news' target='_blank' className="p-2 md:p-4 w-10 h-10 md:h-fit md:w-fit bg-white rounded-full outer-circle hover:scale-125 cursor-pointer transition-all duration-300 hover:bg-[#2CEEC2] group">
                     <Icon icon="bxl:telegram" className="text-black h-6 w-6 md:h-8 md:w-8"/>
-                </div>
-                <div className="p-2 md:p-4 w-10 h-10 md:h-fit md:w-fit bg-white rounded-full outer-circle hover:scale-125 cursor-pointer transition-all duration-300 hover:bg-[#2CEEC2] group">
+                </a>
+                <a href='https://www.instagram.com/thecomplex.agency/' target='_blank' className="p-2 md:p-4 w-10 h-10 md:h-fit md:w-fit bg-white rounded-full outer-circle hover:scale-125 cursor-pointer transition-all duration-300 hover:bg-[#2CEEC2] group">
                     <Icon icon="ri:instagram-line" className="text-black h-6 w-6 md:h-8 md:w-8"/>
-                </div>
-                <div className="p-2 md:p-4 w-10 h-10 md:h-fit md:w-fit bg-white rounded-full outer-circle hover:scale-125 cursor-pointer transition-all duration-300 hover:bg-[#2CEEC2] group">
+                </a>
+                <a href='mailto:thecomplexagency@gmail.com' className="p-2 md:p-4 w-10 h-10 md:h-fit md:w-fit bg-white rounded-full outer-circle hover:scale-125 cursor-pointer transition-all duration-300 hover:bg-[#2CEEC2] group">
                     <Icon icon="basil:gmail-outline" className="text-black h-6 w-6 md:h-8 md:w-8"/>
-                </div>
+                </a>
             </div>
             <div className="text-center mt-14">
-                <div className="text-white/70 text-md">© 2023-2024, Complex Agency - <a className="text-[#2CEEC2]" href="mailto:contact@complex.agency">contact@complex.agency</a></div>
+                <div className="text-white/70 text-md">© 2023-2024, Complex Agency - <a className="text-[#2CEEC2]" href="mailto:thecomplexagency@gmail.com">thecomplexagency@gmail.com</a></div>
                 <div className="text-xs text-white/40 mt-2">Політика сайту та обробка персональних даних</div>
             </div>
         </footer>
