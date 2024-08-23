579d3d99277d21585c423bc44cac51ac16169378
Andrii Zatsepin <uzatsepin@gmail.com>
2024-08-23 08:36:32 +0300

    changed title, descr, metatags, keywords and enabled robots.txt

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,10 @@ import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    metadataBase: new URL("https://complex-agency-next.vercel.app"), // Установите ваш домен здесь
-    title: "Complex Agency – комплексні рішення для твого бізнесу",
-    description: "Ми - агенство цифрових рішень під ключ, від дизайну до розробки, ми виконаємо твое побажання.",
+    metadataBase: new URL("https://www.the-complex.agency/"),
+    title: "Complex Agency – комплексні рішення для твого бізнесу. Створення сайтів, лендінгів, розробка веб-додатків",
+    description: "Ми - агенство цифрових рішень під ключ, від дизайну до розробки вашого вебсайту, ми виконаємо твое побажання у сфері веб-розробки.",
+    keywords: ['website', 'landing', 'web', 'development', 'design', 'complex', 'agency', 'digital', 'solutions', 'business', 'javascript', 'seo', 'vue', 'vuejs', 'react', 'reactjs', 'html', 'css', 'scss', 'sass', 'next'],
     icons: {
         icon: "/favicon.png"
     },
@@ -21,7 +22,7 @@ export const metadata: Metadata = {
         type: "website"
     },
     robots: {
-        index: false,
+        index: true,
         follow: true,
         nocache: true,
         googleBot: {
